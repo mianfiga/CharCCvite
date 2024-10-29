b80e0a902c5a545036250c4b01a78c6e667adf8e
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-10-29 10:36:59 +0000

    renamed:    public/Menu_bar_icons/selected/explore.svg -> public/icons/Menu_bar_icons/selected/explore.svg 	renamed:    public/Menu_bar_icons/selected/home.svg -> public/icons/Menu_bar_icons/selected/home.svg 	renamed:    public/Menu_bar_icons/selected/library.svg -> public/icons/Menu_bar_icons/selected/library.svg 	renamed:    public/Menu_bar_icons/selected/new.svg -> public/icons/Menu_bar_icons/selected/new.svg 	renamed:    public/Menu_bar_icons/selected/settings.svg -> public/icons/Menu_bar_icons/selected/settings.svg 	renamed:    public/Menu_bar_icons/unselected/explore.svg -> public/icons/Menu_bar_icons/unselected/explore.svg 	renamed:    public/Menu_bar_icons/unselected/home.svg -> public/icons/Menu_bar_icons/unselected/home.svg 	renamed:    public/Menu_bar_icons/unselected/library.svg -> public/icons/Menu_bar_icons/unselected/library.svg 	renamed:    public/Menu_bar_icons/unselected/new.svg -> public/icons/Menu_bar_icons/unselected/new.svg 	renamed:    public/Menu_bar_icons/unselected/settings.svg -> public/icons/Menu_bar_icons/unselected/settings.svg 	modified:   src/helpers/icons.js

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,11 +1,11 @@
-export const NEW = "/Menu_bar_icons/unselected/new.svg";
-export const HOME = "/Menu_bar_icons/unselected/home.svg";
-export const EXPLORE = "/Menu_bar_icons/unselected/explore.svg";
-export const LIBRARY = "/Menu_bar_icons/unselected/library.svg";
-export const SETTINGS = "/Menu_bar_icons/unselected/settings.svg";
+export const NEW = "/icons/Menu_bar_icons/unselected/new.svg";
+export const HOME = "/icons/Menu_bar_icons/unselected/home.svg";
+export const EXPLORE = "/icons/Menu_bar_icons/unselected/explore.svg";
+export const LIBRARY = "/icons/Menu_bar_icons/unselected/library.svg";
+export const SETTINGS = "/icons/Menu_bar_icons/unselected/settings.svg";
 
-export const SELNEW = "/Menu_bar_icons/selected/new.svg";
-export const SELHOME = "/Menu_bar_icons/selected/home.svg";
-export const SELEXPLORE = "/Menu_bar_icons/selected/explore.svg";
-export const SELLIBRARY = "/Menu_bar_icons/selected/library.svg";
-export const SELSETTINGS = "/Menu_bar_icons/selected/settings.svg";
+export const SELNEW = "/icons/Menu_bar_icons/selected/new.svg";
+export const SELHOME = "/icons/Menu_bar_icons/selected/home.svg";
+export const SELEXPLORE = "/icons/Menu_bar_icons/selected/explore.svg";
+export const SELLIBRARY = "/icons/Menu_bar_icons/selected/library.svg";
+export const SELSETTINGS = "/icons/Menu_bar_icons/selected/settings.svg";
