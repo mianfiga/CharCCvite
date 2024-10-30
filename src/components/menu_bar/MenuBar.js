3acdac77b428c00fdc38d4d25d28c34d83c481ff
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-10-30 12:10:19 +0000

    cambio menubar

--- src/components/menu_bar/MenuBar.js
+++ src/components/menu_bar/MenuBar.js
@@ -1,5 +1,5 @@
 import "./MenuBar.css"
-import { NEW, EXPLORE, LIBRARY, SETTINGS, TNEW, THOME, TEXPLORE, SELHOME, TLIBRARY, TSETTINGS, SELNEW, SELEXPLORE, SELLIBRARY, SELSETTINGS, HOME } from "../../helpers/menu_bar_icons.js"
+import { NEW, EXPLORE, LIBRARY, SETTINGS, TNEW, THOME, TEXPLORE, SELHOME, TLIBRARY, TSETTINGS, SELNEW, SELEXPLORE, SELLIBRARY, SELSETTINGS } from "../../helpers/menu_bar_icons.js"
 import MenuBarButton from "../menu_bar_button/MenuBarButton.js";
 
 function MenuBar() {
