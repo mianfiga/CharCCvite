32982e3d717c4f5de2e539f10725f4592d91cb72
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-10-30 16:04:17 +0000

    commit a las 16:04

--- src/components/menu_bar/MenuBar.js
+++ src/components/menu_bar/MenuBar.js
@@ -1,5 +1,5 @@
 import "./MenuBar.css"
-import { NEW, EXPLORE, LIBRARY, SETTINGS, TNEW, THOME, TEXPLORE, SELHOME, TLIBRARY, TSETTINGS, SELNEW, SELEXPLORE, SELLIBRARY, SELSETTINGS } from "../../helpers/menu_bar_icons.js"
+import { HOME, NEW, EXPLORE, LIBRARY, SETTINGS, TNEW, THOME, TEXPLORE, SELHOME, TLIBRARY, TSETTINGS, SELNEW, SELEXPLORE, SELLIBRARY, SELSETTINGS } from "../../helpers/menu_bar_icons.js"
 import MenuBarButton from "../menu_bar_button/MenuBarButton.js";
 
 function MenuBar() {
@@ -8,7 +8,7 @@ function MenuBar() {
             <div className="bar_container">
                 <MenuBarButton image={NEW} text={TNEW} />
                 <MenuBarButton image={EXPLORE} text={TEXPLORE} />
-                <MenuBarButton image={SELHOME} text={THOME} />
+                <MenuBarButton image={HOME} text={THOME} />
                 <MenuBarButton image={LIBRARY} text={TLIBRARY} />
                 <MenuBarButton image={SETTINGS} text={TSETTINGS} />
             </div>
