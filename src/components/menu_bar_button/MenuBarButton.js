50479828454943fa9cc98bd36752976a51ea15a1
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-10-30 12:08:26 +0000

    modified:   public/icons/Menu_bar_icons/selected/library.svg 	modified:   public/icons/Menu_bar_icons/selected/new.svg 	modified:   public/icons/Menu_bar_icons/unselected/library.svg 	modified:   src/components/menu_bar/MenuBar.js 	modified:   src/components/menu_bar_button/MenuBarButton.css 	modified:   src/components/menu_bar_button/MenuBarButton.js

--- src/components/menu_bar_button/MenuBarButton.js
+++ src/components/menu_bar_button/MenuBarButton.js
@@ -3,8 +3,10 @@ import "./MenuBarButton.css";
 function MenuBarButton({ image, text }) {
   return (
     <>
-      <div className="image_container">
-        <img src={image} alt="add" />
+      <div className="bar_button_container">
+        <div className="bar_image_container">
+          <img src={image} alt="add" />
+        </div>
         <span>{text}</span>
       </div>
     </>
