fb8b8eb5327af295065cc2577f6e5182807fb0b4
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-11-04 10:21:31 +0000

    throws casi terminado - queda botones

--- src/components/throws_content/ThrowsContent.jsx
+++ src/components/throws_content/ThrowsContent.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import "./ThrowsContent.css";
 import StatButton from "../stat_button/StatButton";
-import { STRENGTH } from "../../helpers/stats_icons";
+import {
+  CHARISMA,
+  CONSTITUTION,
+  DEXTERITY,
+  INTELLIGENCE,
+  STRENGTH,
+  WISDOM,
+} from "../../helpers/stats_icons";
+import { PREVIOUS } from "../../helpers/utility_icons";
 
 function ThrowsContent() {
   return (
     <>
       <div className="throws_content_container">
+        <div className="previous_container">
+          <img src={PREVIOUS} alt="img" />
+        </div>
         <span className="stats_based_text">STATS BASED THROWS</span>
         <div className="stats_based_throws">
-            <StatButton statIcon={STRENGTH} statValue={15} />
+          <StatButton statIcon={STRENGTH} statValue={15} />
+          <StatButton statIcon={DEXTERITY} statValue={16} />
+          <StatButton statIcon={CONSTITUTION} statValue={13} />
+          <StatButton statIcon={INTELLIGENCE} statValue={13} />
+          <StatButton statIcon={WISDOM} statValue={10} />
+          <StatButton statIcon={CHARISMA} statValue={8} />
         </div>
+        <span className="death_throw_text">DEATH SAVING THROW</span>
+        <div className="death_throw_button">THROW</div>
       </div>
     </>
   );
