4f271621af019f48beeb680d156e930892632c90
victorgomezfernandez <victorgomezfernandez@alumno.ieselrincon.es>
2024-11-09 13:41:29 +0000

    prueba dropdown, no es mobile asi que a ver que pasa

--- src/components/actions_content/ActionsContent.jsx
+++ src/components/actions_content/ActionsContent.jsx
@@ -1,11 +1,72 @@
 import React from "react";
 import { PREVIOUS } from "../../helpers/utility_icons";
 import "./ActionsContent.css";
+import { DownOutlined, SmileOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+
+const items = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item (disabled)
+      </a>
+    ),
+    icon: <SmileOutlined />,
+    disabled: true,
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        3rd menu item (disabled)
+      </a>
+    ),
+    disabled: true,
+  },
+  {
+    key: "4",
+    danger: true,
+    label: "a danger item",
+  },
+];
 
 function ActionsContent() {
   return (
     <>
       <div className="actions_content_container">
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              Hover me
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
       </div>
     </>
   );
